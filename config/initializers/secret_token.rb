35b0fdeaec76f9958159a413e7f84b9def07b8ca
jrgrez <joaquingrez@gmail.com>
2014-06-03 13:15:35 -0400

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Katemu::Application.config.secret_key_base = '543dbcec5d7f0835e3664ebb2abb04ce9b5f6424ffe2285cc6d5ef3754c9937df6a992341d3178ac8437408d4133b357ee0683cef7a98821a24543bd0221d37f'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Katemu::Application.config.session_store :cookie_store, key: '_katemu_session'
