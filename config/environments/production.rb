e1721c927523c6e71ac3c39fbad8d29291bba889
jrgrez <joaquingrez@gmail.com>
2014-06-05 12:13:43 -0400

    Compile true in production enviroment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,7 @@ Katemu::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+  config.assets.compile=true
+
 end
