def3f51b71bbe66ef30cd37c609a6a6394ab7a41
jrgrez <joaquingrez@gmail.com>
2014-06-10 09:29:33 -0400

    iPhone landscape view blocked

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -38,23 +38,3 @@ $("a.popup").click(function(e) {
     e.stopPropagation(); return false;
 });
 
-$(document).ready(function () {
-    $(window)
-        .bind('orientationchange', function(){
-            if (window.orientation % 180 == 0){
-                $(document.body).css("-webkit-transform-origin", "")
-                    .css("-webkit-transform", "");
-            }
-            else {
-                if ( window.orientation > 0) { //clockwise
-                    $(document.body).css("-webkit-transform-origin", "200px 190px")
-                        .css("-webkit-transform",  "rotate(-90deg)");
-                }
-                else {
-                    $(document.body).css("-webkit-transform-origin", "280px 190px")
-                        .css("-webkit-transform",  "rotate(90deg)");
-                }
-            }
-        })
-        .trigger('orientationchange');
-});
--- app/assets/javascripts/static_pages.js.coffee
+++ app/assets/javascripts/static_pages.js.coffee
@@ -1,3 +1,18 @@
 # Place all the behaviors and hooks related to the matching controller here.
 # All this logic will automatically be available in application.js.
 # You can use CoffeeScript in this file: http://coffeescript.org/
+
+$(window).bind 'orientationchange', ->
+  if window.orientation % 180 == 0
+    $(document.body).css
+      "-webkit-transform-origin" : ''
+      "-webkit-transform" : ''
+  else
+    if window.orientation > 0
+      $(document.body).css
+        "-webkit-transform-origin" : "200px 190px"
+        "-webkit-transform" : "rotate(-90deg)"
+    else
+      $(document.body).css
+        "-webkit-transform-origin" : "280px 190px"
+        "-webkit-transform" : "rotate(90deg)"
