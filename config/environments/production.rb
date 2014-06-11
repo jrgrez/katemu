f73f346bafc79c6bf4c71a8c4e613667b09d0c3d
jrgrez <joaquingrez@gmail.com>
2014-06-11 16:36:23 -0400

    a!

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -27,7 +27,7 @@ Katemu::Application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
 
   # Generate digests for assets URLs.
   config.assets.digest = true
@@ -78,9 +78,7 @@ Katemu::Application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
-  config.assets.compile=true
   config.cache_classes = true
   config.serve_static_assets = true
-  config.assets.digest = true
 
 end
